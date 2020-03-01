68e74efc0dbbe25eec6db0cc7a5443014a3b2b55
maralbe <m.behvandi@azki.com>
2020-03-01 16:07:51 +0330

    fix namespace, add trackPurchase() function

--- src/iTakhfifanApiClient.php
+++ src/iTakhfifanApiClient.php
@@ -1,9 +1,15 @@
 <?php
 
+namespace thirdparty\takhfifan;
+
+
 /**
  * Interface iTakhfifanApiClient
+ *
+ * @package thirdparty\takhfifan
  */
 interface iTakhfifanApiClient
 {
 
+    function trackPurchase(string $token, string $order_id, int $amount, int $status, bool $is_new_customer, float $coupon_discount, float $tax, float $shipping_amount);
 }
