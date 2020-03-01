613aec2ace0b4279af4359f6b9438c17b811b162
maralbe <m.behvandi@azki.com>
2020-03-01 16:06:15 +0330

    Add ApiResponseException class

---
+++ src/Exception/ApiResponseException.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace thirdparty\takhfifan\Exception;
+
+
+/**
+ * Class ApiResponseException
+ *
+ * @package thirdparty\takhfifan\Exception
+ */
+class ApiResponseException extends BaseException
+{
+
+}
