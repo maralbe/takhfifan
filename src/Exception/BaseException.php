706d94d4ab1249b62e83c50306831aab5e4146b8
maralbe <m.behvandi@azki.com>
2020-03-01 16:06:34 +0330

    fix namespace

--- src/Exception/BaseException.php
+++ src/Exception/BaseException.php
@@ -1,7 +1,14 @@
 <?php
 
+namespace thirdparty\takhfifan\Exception;
+
+
+use Exception;
+
 /**
  * Class BaseException
+ *
+ * @package thirdparty\takhfifan\Exception
  */
 class BaseException extends Exception
 {
